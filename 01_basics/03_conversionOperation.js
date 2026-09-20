b4db2b235768727b64c1ebb327002944b4663a8a
Atul Anand <atulanand092004@gmail.com>
2026-09-20 21:14:26 +0530

    Add explanations and examples for string concatenation, increment/decrement operations, and ToPrimitive conversion in JavaScript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -56,7 +56,7 @@ console.log(typeof stringSomeNumber);
 // what is Operations? Operations are the actions that can be performed on variables or expressions. They are used to manipulate data and perform calculations. The operations can be arithmetic, comparison, logical, or assignment operations. Arithmetic operations include addition, subtraction, multiplication, division, and modulus. Comparison operations include equal to, not equal to, greater than, less than, greater than or equal to, and less than or equal to. Logical operations include AND, OR, and NOT. Assignment operations include assigning a value to a variable using the assignment operator (=) or compound assignment operators (+=, -=, *=, /=, %=).
 
 let value = 5;
-let negvalue = -value; // negitive value of value
+let negvalue = -value; // negative value of value
 
 console.log(negvalue) // -5
 console.log(typeof negvalue);
@@ -72,4 +72,54 @@ console.log(3 ** 4) // Exponentiation operation. It will return 81. Exponentiati
 console.log(5 / 3) // Division operation. It will return 1.6666666666666667. Division operation is used to divide one number by another number.
 
 console.log(5 % 3) // Modulus operation. It will return 2. Modulus operation is used to find the remainder of a division operation.
+
+let str1 = "Atul";
+let str2 = " Anand";
+console.log(str1 + str2) // Concatenation operation. It will return "Atul Anand". Concatenation operation is used to combine two or more strings together.
+
+// But we not subtract two strings. It will give NaN because it is not a valid operation. It is used to indicate that a variable has not been assigned a value. It is often used as a placeholder for a variable that will be assigned later.
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// This is only possiable because of ToPrimitive Operation. ToPrimitive is a internal operation that converts an object to a primitive value. It is used to convert an object to a primitive value when an operation is performed on the object. The ToPrimitive operation can be performed using the valueOf() and toString() methods of the object. The valueOf() method returns the primitive value of the object, while the toString() method returns a string representation of the object. If the object does not have a valueOf() or toString() method, the ToPrimitive operation will return NaN.
+
+
+// ToPrimitive is input[, preferredType] => output. because it takes input and preferredType and returns output. It is used to convert an object to a primitive value when an operation is performed on the object. The input is the object that is being converted, the preferredType is the type of primitive value that is desired, and the output is the primitive value that is returned. The preferredType can be either "String" or "number". If the preferredType is "String", the ToPrimitive operation will return a string representation of the object. If the preferredtype is "number", the ToPrimitive operation will return a number representations of the object. If the preferredType is not specified, the ToPrimitive operation will return a number representation of the object by default.
+
+
+console.log( (3 +2) * 2 % 3); // it is the best way to write a code. Or represent the code in a better way.
+
+console.log(+true) // it will return 1. because true is a boolean value and it is converted to number using ToPrimitive operation. The ToPrimitive operation converts the boolean value to a number value. The boolean value true is converted to the number value 1, and the boolean value false is converted to the number value 0.
+
+console.log(+false) // it will return 0. because false is a boolean value and it is converted to number using ToPrimitive operation. The ToPrimitive operation converts the boolean value to a number value. The boolean value true is converted to the number value 1, and the boolean value false is converted to the number value 0.
+
+console.log(+"") // it will return 0. because empty string is a string value and it is converted to number using ToPrimitive operation. The ToPrimitive operation converts the string value to a number value. The empty string is converted to the number value 0, and any non-empty string is converted to the number value NaN.
+
+
+let num1, num2, num3;
+ num1 = num2 = num3 = 2 + 2;
+
+
+ let gameCounter = 100;
+
+    gameCounter++; // it will increment the value of gameCounter by 1. It is used to increase the value of a variable by 1. It can also be used to decrease the value of a variable by 1 using the -- operator.
+    
+    // It is also called as a Postfix Increment Operator. It is used to increase the value of a variable by 1 after the current operation is performed. It can also be used to decrease the value of a variable by 1 after the current operation is performed using the -- operator.
+
+    gameCounter--; // it will decrement the value of gameCounter by 1. It is used to decrease the value of a variable by 1. It can also be used to increase the value of a variable by 1 using the ++ operator.
+
+    ++gameCounter; // it will increment the value of gameCounter by 1. It is used to increase the value of a variable by 1 before the current operation is performed. It can also be used to decrease the value of a variable by 1 before the current operation is performed using the -- operator.
+
+    // It is also called as a Prefix Increment Operator. It is used to increase the value of a variable by 1 before the current operation is performed. It can also be used to decrease the value of a variable by 1 before the current operation is performed using the -- operator.
+
+    --gameCounter; // it will decrement the value of gameCounter by 1. It is used to decrease the value of a variable by 1 before the current operation is performed. It can also be used to increase the value of a variable by 1 before the current operation is performed using the ++ operator.
+
+    // It is also called as a Prefix Decrement Operator. It is used to decrease the value of a variable by 1 before the current operation is performed. It can also be used to increase the value of a variable by 1 before the current operation is performed using the ++ operator.
+    
+
+    
+    console.log(gameCounter); 
 
