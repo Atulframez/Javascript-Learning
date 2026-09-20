e98e4ae26409db5bd97f9bcf9faa01b5a9f0dffc
Atul Anand <atulanand092004@gmail.com>
2026-09-20 13:23:51 +0530

    Add-variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 144553
+let accountEmail = "Atul@github.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState;
+
+
+
+// accountId = 2 // Not allowed because accountId is a constant. 
+
+// What is scpoe? Scope is the current context of code, which determines the accessibility of variables to JavaScript.
+    
+/*
+Prefer not to use var, because it has a function scope. Use let and const instead, because they have block scope.
+and block scope is more predictable than function scope.
+
+*/
+
+
+
+acountEmail = "habh@gmail.com"
+accountPassword = "123456"
+acountCity = "Pune"
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
