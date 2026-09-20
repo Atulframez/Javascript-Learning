2d017f4e51d38511b0c5741f90089a5fff8d1c70
Atul Anand <atulanand092004@gmail.com>
2026-09-20 18:05:15 +0530

    Add explanations for null and undefined cases in type conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,11 @@
 let score = "33"; // let score = "33atul"; // this will give NaN because "33atul" is not a valid number. It is a string data type. string is a sequence of characters. It is used to represent text. It is enclosed in single quotes, double quotes, or backticks.
 
+// what is about Null case? Null case is a special case where the value of a variable is null. Qwhat will be conversion of null to number? it will be 0. beacause null is considered as an empty value. It is used to indicate that a variable has no value or that an object property does not exist. It is often used as a placeholder for an object that will be assigned later.
+
+// what is about undefined case? Undefined case is a special case where the value of a variable is undefined. What will be conversion of undefined to number? it will be NaN because undefined is not a valid number. It is used to indicate that a variable has not been assigned a value. It is often used as a placeholder for a variable that will be assigned later.
+
+
+
 console.log(score + 1) // "331"
 console.log(typeof score);
 console.log(typeof (score));
