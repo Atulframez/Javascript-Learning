d0765930e26ea71f549cdce583aa77bfe09f5e3c
Atul Anand <atulanand092004@gmail.com>
2026-09-22 10:52:45 +0530

    Add explanations and examples for comparison operators and data types in JavaScript

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -22,3 +22,18 @@ console.log(3 === 1); // this is strict equal to operator. It will return false 
 console.log(3 !== 1); // this is strict not equal to operator. It will return true because 3 is not strictly equal to 1. It is used to compare two values and return a boolean value (true or false) based on the comparison.
 
 // What is Comparison Operators? Comparison operators are used to compare two values and return a boolean value (true or false) based on the comparison. They are used in conditional statements to control the flow of a program based on certain conditions. The comparison operators in JavaScript are: equal to (==), not equal to (!=), strict equal to (===), strict not equal to (!==), greater than (>), less than (<), greater than or equal to (>=), and less than or equal to (<=).
+
+
+// This is a special case where the value of a variable is string. What will be conversion of string to number? it will be NaN if the string is not a valid number. It is used to represent text. It is enclosed in single quotes, double quotes, or backticks. 
+
+console.log("2" > 1);
+console.log("02" > 1); 
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null !== 0); 
+
+// What is null and 0 comparison? Null is a special value that represents the absence of any object value. It is used to indicate that a variable has no value or that an object property does not exist. It is often used as a placeholder for an object that will be assigned later. 0 is a number data type. Number is used to represent numeric values. It can be an integer or a floating-point number. When comparing null and 0, null is considered to be less than 0, but it is not equal to 0. This is because null represents the absence of a value, while 0 represents a numeric value. Therefore, when comparing null and 0, null is considered to be less than 0, but it is not equal to 0.
+
---
+++ 01_basics/05_datatypes02.js
@@ -0,0 +1,15 @@
+// How many types of data types are there in JS? There are 7 primitive data types and 3 non-primitive data types in JavaScript. The primitive data types are: string, number, boolean, undefined, null, symbol, and BigInt. The non-primitive data types are: object, array, and function. 
+
+// What is symbol? Symbol is a primitive data type that represents a unique and immutable value. It is often used as a key for object properties to avoid name collisions. Each time you create a new symbol, it is guaranteed to be unique.
+
+// What is BigInt? BigInt is a primitive data type that can represent integers with arbitrary precision. It is used to store large integers that cannot be represented by the Number data type. It is created by appending 'n' to the end of an integer literal or by calling the BigInt() constructor.
+
+// What is null? Null is a special value that represents the absence of any object value. It is used to indicate that a variable has no value or that an object property does not exist. It is often used as a placeholder for an object that will be assigned later. 
+
+// Primitives data types are call by value. It means that when we assign a primitive value to a variable, the variable stores the actual value of the primitive data types. When we assign a primitive value to another variable, the new variable gets a copy of the value, and any changes made to the new variable do not affect the orginal variable.
+
+// Non-primitives data types are call by reference. It means that when we assign a non-primitive value to a variable, the variable stores a reference to the value of the non-primitive data types. When we assign a non-primitive value to another variable, the new variable gets a reference to the same value, and any changes made to the new variable affect the orginal variables. 
+
+
+
+
